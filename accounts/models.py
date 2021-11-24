023d3cc28241acff06b87027cbeb4deebc7a44fc
manucho254 <manuchoadero@gmail.com>
2021-11-24 19:27:36 +0300

    initial

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    pass
+
+class Profile(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    profile_image = models.ImageField(upload_to="images")
+    
+    def __str__(self):
+        return "{}  {}".format(self.firstname,  self.lastname)
