3dd8260dfc6ef0aed0ad1987877c97f9e670ae72
manucho254 <manuchoadero@gmail.com>
2021-11-24 14:24:08 +0300

    initial

---
+++ core/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Users(models.Model):
+    pass
+
+class Profile(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    profile_image = models.ImageField(upload_to="images")
+    
+    def __str__(self):
+        return "{}  {}".format(self.firstname,  self.lastname)
+
+class Post(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE,  related_name="author")
+    title = models.CharField(max_length=200,  blank=True, null=True)
+    post_image = models.ImageField(upload_to="images")
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return "{} posted {}".format(self.author, self.title) 
+
+class Comment(models.Model):
+    message_by = models.ForeignKey(Profile,  on_delete=models.CASCADE)
+    message = models.TextField(max_length=200)
+    commented_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.message_by
+    
+    class Meta:
+        ordering = ["-commented_on"]
