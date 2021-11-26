8e7cd863ff5c712a5acb1b2e78b59cbf368b0705
manucho254 <manuchoadero@gmail.com>
2021-11-26 18:24:24 +0300

    post

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Comment
+from .models import Comment,Post
 
 class CommentModelForm(forms.ModelForm):
     message = forms.CharField(label ="", widget = forms.TextInput (attrs ={
@@ -11,3 +11,14 @@ class CommentModelForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["message"]
+        
+class PostModelForm(forms.ModelForm):
+    body = forms.CharField(label ="", widget = forms.TextInput (attrs ={
+        'placeholder':'Add post',
+        'class': "post-body w-75",
+        'rows': "3",
+        "id": "body"
+        }))
+    class Meta:
+        model = Post
+        fields = ["caption", "body",]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import Post,Comment
 from django.contrib import messages
-from .forms import CommentModelForm
+from .forms import CommentModelForm,PostModelForm
 from django.views import View
+from django.views.generic import CreateView,UpdateView,DeleteView
 
 
 class LandingPage(View):
@@ -12,8 +13,27 @@ class LandingPage(View):
 class HomeView(View):
     def get(self,  request,  *args,  **kwargs):
         posts = Post.objects.all()
+        form = PostModelForm()
         context = {
             "posts": posts,
+            "form": form
         }
         return render(request,  "homepage.html",  context)
+    
+    def post(self,  request,  *args,  **kwargs):
+        posts = Post.objects.all()
+        form = PostModelForm(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+            
+        context = {
+            "posts": posts,
+            "form":form
+        }
+        return render(request,  "homepage.html",  context)
+    
+class CreatePost(CreateView):
+    pass
     
