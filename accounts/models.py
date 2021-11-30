cccb6021111f70c3462c5b9710b89a8f2081fd8b
manucho254 <manuchoadero@gmail.com>
2021-11-30 19:37:00 +0300

    user profiles

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Users(models.Model):
     pass
 
 class Profile(models.Model):
-    username = models.CharField(max_length=40)
-    firstname = models.CharField(max_length=200)
-    lastname = models.CharField(max_length=200)
-    profile_image = models.ImageField(upload_to="images",  null=True, blank=True)
-    
+    user = models.OneToOneField(User, primary_key=True, verbose_name='user', related_name='profile',  on_delete=models.CASCADE)
+    username = models.CharField(max_length=100, blank=True, null=True)
+    first_name = models.CharField(max_length=200, blank=True, null=True)
+    last_name = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.CharField(max_length=300, blank=True, null=True)
+    birthday = models.DateField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    profile_image = models.ImageField(upload_to="profile/",  null=True, blank=True)
+    profile_created_on = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return "{}".format(self.username)
