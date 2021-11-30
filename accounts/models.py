b8481069d1315f7629ef2616c3be9d638722ae15
manucho254 <manuchoadero@gmail.com>
2021-11-30 19:57:46 +0300

    Profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Users(models.Model):
-    pass
+GENDER_CHOICES = (
+    ("male", "male"),
+    ("female", "female")
+    )
 
 class Profile(models.Model):
     user = models.OneToOneField(User, primary_key=True, verbose_name='user', related_name='profile',  on_delete=models.CASCADE)
@@ -12,6 +14,7 @@ class Profile(models.Model):
     bio = models.CharField(max_length=300, blank=True, null=True)
     birthday = models.DateField(max_length=200, blank=True, null=True)
     location = models.CharField(max_length=200, blank=True, null=True)
+    gender = models.CharField(max_length = 20, choices = GENDER_CHOICES, default = 'Enter Gender')
     profile_image = models.ImageField(upload_to="profile/",  null=True, blank=True)
     profile_created_on = models.DateTimeField(auto_now_add=True)
 
