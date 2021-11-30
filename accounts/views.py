e7bdb761213d2f3c5757859a59ced5b8cda61d43
manucho254 <manuchoadero@gmail.com>
2021-11-30 21:53:46 +0300

    signals

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.urls import reverse_lazy
 from .models import Profile
 from core.models import  Post
+from django.db.models.signals import post_save
+from django.dispatch import reciever
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views import View
 from django.views.generic import UpdateView,DeleteView
@@ -18,4 +20,16 @@ class ProfileView(LoginRequiredMixin, View):
             'posts' : posts
         }
         
-        return render(request,  "profile.html",  context)
+        return render(request,  "profiles/profile.html",  context)
+    
+class EditProfile(View):
+    def get(self,  request, pk,*args,  **kwargs):
+        profile = Profile.objects.get(pk=pk)
+        user = profile.user
+        
+        context = {
+            'profile': profile,
+            'user': user,
+        }
+        
+        return render(request,  "profiles/edit_profile.html",  context)
--- manuchosocial/urls.py
+++ manuchosocial/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  include("core.urls")),
+    path('', include('accounts.urls')),
     path('auth/', include('allauth.urls')),
 ]
 if settings.DEBUG:
