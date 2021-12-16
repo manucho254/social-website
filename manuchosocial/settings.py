6d4f2339e2dfb7661cea89d6887d7716a5eb08ac
manucho254 <manuchoadero@gmail.com>
2021-12-16 14:47:40 +0300

    postgres

--- manuchosocial/settings.py
+++ manuchosocial/settings.py
@@ -4,7 +4,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -14,7 +13,7 @@ SECRET_KEY = 'django-insecure-+w!wucr14o*vq%e9jitfsdlfr4^q*5=ng*p4e0-sa$2u$&nio0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Authentication backend
 
@@ -87,8 +86,12 @@ WSGI_APPLICATION = 'manuchosocial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'socialweb',
+        'USER': 'postgres',
+        'PASSWORD': 'manucho',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
